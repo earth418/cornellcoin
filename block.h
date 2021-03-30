834e3e8dbc529a3203f2f53a34b595474f9831d9
= <alihafez42@gmail.com>
2021-03-30 15:09:23 -0400

    Added some constructors to the block and transaction classes, & some documentation

--- block.h
+++ block.h
@@ -5,8 +5,23 @@
 class Block
 {
 
-    const char* previousBlockHash;
+    const int NUM_TRANSACTIONS = 256;
 
-    const Transaction* transactionList;
+    const char* precedingBlockHash;
+
+    Transaction* transactionList = (Transaction*) malloc(sizeof(Transaction) * NUM_TRANSACTIONS); // just in case we actually wanna do C for some reason
+
+    char* blockKey; // When the proof-of-work is found, store it in the blockKey.
+
+public:
+
+    /** 
+    * Called when the proof-of-work has been found for a finished block
+    * A finished block has a complete list of NUM_TRANSACTIONS transactions
+    **/
+   
+    Block(const char* previousBlockHash, const Transaction* listOfTransactionsToAdd, const char* proofOfWork)
+    : precedingBlockHash(previousBlockHash), transactionList(listOfTransactionsToAdd), blockKey(proofOfWork)
+    {}
 
 }
--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
-"#include <iostream>" 
+#include <iostream>
+#include "block.h"
+
+#pragma once
+
+int main()
+{
+
+    std::cout << "I don't know why I made this file, do you?" << std::endl;
+
+    return 0;
+}
