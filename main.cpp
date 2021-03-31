6ef11211365a6d6616601f5df788e3254b1fd038
Camto <www.ben.pr@gmail.com>
2021-03-31 11:35:43 -0400

    Change to bitset

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <bitset>
 #include "block.h"
 
 class Blockchain
@@ -21,14 +22,14 @@ class Blockchain
     * Pass in a value that has been hashed, to check if it is a valid proof-of-work solution.
     * Precondition: inputKey is a 256-length string of ones and zeros. Can be passed as a bool pointer, or even one char. 0 < inputKey < 0xff
     */
-    bool isValidKey(const char* inputKey)
+    bool isValidKey(const std::bitset<256> inputKey)
     {
         // I don't know what our condition is
         // Let's just for now say that it has to start with 40 zeroes
 
         for (int i = 0; i < 40; ++i)
         {
-            if (*(inputKey + i) == '1')
+            if (inputKey[i] == 1)
                 return false;
         }
 
